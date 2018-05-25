2a1ffcd6e6aa3605a0d2d01d32566b146929b241
LH <lh@lhdv.net>
2018-05-25 13:26:09 -0300

    Changes in solution for better performance of grains exercise

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,7 +2,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square calculate how many wheat grains there are in a 8x8 chess board
@@ -11,18 +10,10 @@ func Square(n int) (uint64, error) {
 		return 0, errors.New("invalid number")
 	}
 
-	square := math.Pow(2, float64(n-1))
-
-	return uint64(square), nil
+	return (1 << (uint64(n) - 1)), nil
 }
 
 // Total sums all the wheat grains a 8x8 chess board has
 func Total() uint64 {
-	var r uint64
-	for i := 1; i <= 64; i++ {
-		s, _ := Square(i)
-		r += s
-	}
-
-	return r
+	return (uint64)((1 << 64) - 1)
 }
