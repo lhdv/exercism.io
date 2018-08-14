93dbaee139ad2da28a567eb6c659efb2ed7f1742
LH <lh@lhdv.net>
2018-08-14 17:28:55 -0300

    First iteration for twelve-days

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -4,9 +4,73 @@ import (
 	"fmt"
 )
 
-type lyric struct {
-	ordinalDay string
-	gift       string
+type ordinal int
+
+func (o ordinal) String() string {
+	switch {
+	case o == 1:
+		return "first"
+	case o == 2:
+		return "second"
+	case o == 3:
+		return "third"
+	case o == 4:
+		return "fourth"
+	case o == 5:
+		return "fifth"
+	case o == 6:
+		return "sixth"
+	case o == 7:
+		return "seventh"
+	case o == 8:
+		return "eighth"
+	case o == 9:
+		return "ninth"
+	case o == 10:
+		return "tenth"
+	case o == 11:
+		return "eleventh"
+	case o == 12:
+		return "twelfth"
+	default:
+		return ""
+	}
+}
+
+type lyricVerse struct {
+	day  int
+	gift string
+}
+
+func (lv lyricVerse) String() string {
+
+	gifts := make(map[int]string)
+	gifts[1] = "a Partridge in a Pear Tree"
+	gifts[2] = "two Turtle Doves"
+	gifts[3] = "three French Hens"
+	gifts[4] = "four Calling Birds"
+	gifts[5] = "five Gold Rings"
+	gifts[6] = "six Geese-a-Laying"
+	gifts[7] = "seven Swans-a-Swimming"
+	gifts[8] = "eight Maids-a-Milking"
+	gifts[9] = "nine Ladies Dancing"
+	gifts[10] = "ten Lords-a-Leaping"
+	gifts[11] = "eleven Pipers Piping"
+	gifts[12] = "twelve Drummers Drumming"
+
+	for i := lv.day; i >= 1; i-- {
+		if i > 1 {
+			lv.gift += gifts[i] + ", "
+		} else {
+			if lv.day > 1 {
+				lv.gift += "and " + gifts[i] + "."
+			} else {
+				lv.gift += gifts[i] + "."
+			}
+		}
+	}
+
+	return fmt.Sprintf("On the %s day of Christmas my true love gave to me, %s", ordinal(lv.day), lv.gift)
 }
 
 // Song print the twelve days song
@@ -24,42 +88,9 @@ func Song() string {
 // Verse return a song's verse based on its number
 func Verse(n int) string {
 
-	var verse string
-	var gifts string
-
 	if n < 1 || n > 12 {
-		return ""
+		return "Wrong verse number"
 	}
 
-	lyrics := make(map[int]lyric)
-	lyrics[1] = lyric{ordinalDay: "first", gift: "a Partridge in a Pear Tree"}
-	lyrics[2] = lyric{ordinalDay: "second", gift: "two Turtle Doves"}
-	lyrics[3] = lyric{ordinalDay: "third", gift: "three French Hens"}
-	lyrics[4] = lyric{ordinalDay: "fourth", gift: "four Calling Birds"}
-	lyrics[5] = lyric{ordinalDay: "fifth", gift: "five Gold Rings"}
-	lyrics[6] = lyric{ordinalDay: "sixth", gift: "six Geese-a-Laying"}
-	lyrics[7] = lyric{ordinalDay: "seventh", gift: "seven Swans-a-Swimming"}
-	lyrics[8] = lyric{ordinalDay: "eighth", gift: "eight Maids-a-Milking"}
-	lyrics[9] = lyric{ordinalDay: "ninth", gift: "nine Ladies Dancing"}
-	lyrics[10] = lyric{ordinalDay: "tenth", gift: "ten Lords-a-Leaping"}
-	lyrics[11] = lyric{ordinalDay: "eleventh", gift: "eleven Pipers Piping"}
-	lyrics[12] = lyric{ordinalDay: "twelfth", gift: "twelve Drummers Drumming"}
-
-	for i := n; i >= 1; i-- {
-		if i > 1 {
-			gifts += lyrics[i].gift + ", "
-		} else {
-			if n > 1 {
-				gifts += "and " + lyrics[i].gift + "."
-			} else {
-				gifts += lyrics[i].gift + "."
-			}
-		}
-
-	}
-
-	verse = fmt.Sprintf("On the %s day of Christmas my true love gave to me, %s", lyrics[n].ordinalDay, gifts)
-
-	return verse
-
+	return lyricVerse{day: n}.String()
 }
